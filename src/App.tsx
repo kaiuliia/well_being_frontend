5628d598a882f051768c73993cbf9517f2accc4d
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-10-29 20:01:50 +0100

    created register components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Register} from './components/registerform'
 
 function App() {
   return (
     <div className="App">
+      <Register title='hrhr'/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/registerform.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    title: string;
+}
+
+
+    export function  Register(props: Props) {
+        return  (
+            <div>{props.title}</div>
+        );
+    };
+
+    // export default Register;
+
