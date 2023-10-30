fb423772d6b970457a8db91dd541d51bd85aa4da
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-10-30 15:38:11 +0100

    upd handlesubmit

--- src/components/registerform.tsx
+++ src/components/registerform.tsx
@@ -4,39 +4,52 @@ interface Props {
     title: string;
 }
 
+interface Users {
+    name: string;
+}
 
     export function  Register(props: Props) {
         const [name, setName] = useState('');
         const [email, setEmail] = useState('');
         const [password, setPassword] = useState('');
+        const [users, setUsers] = useState([{'name': 'anna'}, {'name': 'alex'}])
 
         // States for checking the errors
         const [submitted, setSubmitted] = useState(false);
         const [error, setError] = useState(false);
 
         // HandleChange method to update the states
-        const handleChange = () => {
-            console.log('eheeh')
+        const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+            setName(e.target.value);
+        }
+
+        const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+
+
+                if (name) {
+                    setUsers((prevUsers) => [...prevUsers, { name: name }]);
+                    setName(''); // Clear the input field
+                }
+
+            if (name === '' || email === '' || password === '') {
+                setError(true);
+            } else {
+                setSubmitted(true);
+                setError(false);
+            }
+            setName('')
+
+            console.log(users)
         }
 
 
 
-        // Handling the form submission
-        // const handleSubmit = (e)  => {
-        //     e.preventDefault();
-        //     if (name === '' || email === '' || password === '') {
-        //         setError(true);
-        //     } else {
-        //         setSubmitted(true);
-        //         setError(false);
-        //     }
-        // };
-
         return (
             <div>
-                <form>
-                    <input  onChange={handleChange}/>
-                    <button onClick={handleChange} className="btn"
+                <form onSubmit={handleSubmit}>
+                    <input  onChange={handleChange} value={name}/>
+                    <button  className="btn"
                             type="submit">
                       Submit
                     </button>
