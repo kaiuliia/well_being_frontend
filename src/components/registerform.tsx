fe0d9c7dc24ac301d7cccefdbf041564346b118c
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-10-30 14:31:19 +0100

    added input form

--- src/components/registerform.tsx
+++ src/components/registerform.tsx
@@ -6,7 +6,13 @@ interface Props {
 
 
     export function  Register(props: Props) {
-        const [name, setName] = useState("");
+        const [name, setName] = useState('');
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+
+        // States for checking the errors
+        const [submitted, setSubmitted] = useState(false);
+        const [error, setError] = useState(false);
 
         // HandleChange method to update the states
         const handleChange = () => {
@@ -15,16 +21,28 @@ interface Props {
 
 
 
+        // Handling the form submission
+        // const handleSubmit = (e)  => {
+        //     e.preventDefault();
+        //     if (name === '' || email === '' || password === '') {
+        //         setError(true);
+        //     } else {
+        //         setSubmitted(true);
+        //         setError(false);
+        //     }
+        // };
 
         return (
             <div>
                 <form>
-                    <input value={name} onChange={handleChange}/>
+                    <input  onChange={handleChange}/>
+                    <button onClick={handleChange} className="btn"
+                            type="submit">
+                      Submit
+                    </button>
                 </form>
             </div>
         )
-
-
 
     };
 
