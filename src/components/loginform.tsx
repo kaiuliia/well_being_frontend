82919657ebdc5caa74f947c1d6bbcbd5772dbe71
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-03 20:25:08 +0100

    login/reg form changes on the button click

--- src/components/loginform.tsx
+++ src/components/loginform.tsx
@@ -6,39 +6,29 @@ interface Props {
 }
 
 interface User {
-  name: string;
   email: string;
   password: string;
 }
 
 export function Login(props: Props) {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [users, setUsers] = useState<User[]>([
-    { name: "anna", email: "anna@gmail.com", password: "fgehd7832-dsjb" },
-    { name: "alex", email: "alex-dd@gmail.com", password: "fgce-wsg" },
-  ]);
 
   // States for checking the errors
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
   // const [visible, setVisible] = useState(true);
-  console.log(users);
 
   // HandleChange method to update the states
 
   const sendData = async (user: User) => {
-    await fetch("http://localhost:9090/register", {
+    await fetch("http://localhost:9090/login", {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(user),
     });
   };
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -47,36 +37,21 @@ export function Login(props: Props) {
     setPassword(e.target.value);
   };
 
-  useEffect(() => {
-    const api = async () => {
-      const data = await fetch("http://localhost:9090/api", {
-        method: "GET",
-      });
-      const jsonData = await data.json();
-      setSubmitted(jsonData.message);
-    };
-
-    api();
-  }, []);
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (name && email && password) {
-      setUsers([...users, { name: name, email: email, password: password }]);
-      await sendData({ name: name, email: email, password: password });
-      setName(""); // Clear the input field
+    if (email && password) {
+      await sendData({ email: email, password: password });
       setEmail("");
       setPassword("");
     }
 
-    if (name === "" || email === "" || password === "") {
+    if (email === "" || password === "") {
       setError(true);
     } else {
       setSubmitted(true);
       setError(false);
     }
-    setName("");
   };
 
   return (
