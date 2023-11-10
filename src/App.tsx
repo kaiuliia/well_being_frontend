0463aab03bb450eeb2c73ac0015b6d3eb9b6b672
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-10 20:55:00 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { Register } from "./components/registerform";
 import { Login } from "./components/loginform";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { MainPage } from "./components/mainpage";
 
 function App() {
@@ -20,27 +20,46 @@ function App() {
     setRegisterVisible(true);
   };
 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login title="hrhr" visible={loginVisible} />} />
-      <Route
-        path="/register/*"
-        element={<Register title="hrhr" visible={registerVisible} />}
-      />
-      <Route path="/main/*" element={<MainPage />} />
-    </Routes>
-  </BrowserRouter>;
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<Login title="hrhr" visible={loginVisible} />} />
+  //     <Route
+  //       path="/register/*"
+  //       element={<Register title="hrhr" visible={registerVisible} />}
+  //     />
+  //     <Route path="/main/*" element={<MainPage />} />
+  //   </Routes>
+  // </BrowserRouter>;
 
   return (
     <div className="App">
       <button onClick={handleLoginClick}>Login</button>
       <button onClick={handleRegisterClick}>Register</button>
-      <Link to="main">main</Link>
-      <Login title="hrhr" visible={loginVisible} />
-      <Register title="hrhr" visible={registerVisible} />
+      <Router>
+        <Login title="hrhr" visible={loginVisible} />
 
-      <MainPage />
+        <Routes>
+          {/* <HomePage /> */}
+          {/*<Route path="/:type/:id" element={<PetDetailsPage />} />*/}
+          <Route
+            path="/register"
+            element={<Register title="hrhr" visible={registerVisible} />}
+          />
+          <Route path="/main" element={<MainPage />} />
+          {/*<Route path="/:type?" element={<HomePage />} />*/}
+        </Routes>
+      </Router>
     </div>
+
+    // <div className="App">
+    //   <button onClick={handleLoginClick}>Login</button>
+    //   <button onClick={handleRegisterClick}>Register</button>
+    //   <Link to="main">main</Link>
+    //   <Login title="hrhr" visible={loginVisible} />
+    //   <Register title="hrhr" visible={registerVisible} />
+    //
+    //   <MainPage />
+    // </div>
   );
 }
 
