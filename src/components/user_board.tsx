aebe3fbc177b4d2a0272e3525586f4801a8354ce
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-15 09:58:28 +0100

    created fetch

--- src/components/user_board.tsx
+++ src/components/user_board.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { SliderBar } from "./slide_bar";
+import { FormEvent, MouseEvent } from "../types";
 
 interface Props {}
 
@@ -27,7 +28,30 @@ export function UserBoard(props: Props) {
     yourself_time: 0,
     screen_time: 0,
   });
+  const [statusMessage, setStatusMessage] = useState("");
+
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
   const name = localStorage.getItem("name");
+
+  const sendData = async (surway: Surway) => {
+    const response = await fetch("http://localhost:9090/login/user", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(surway),
+    });
+    if (response.status > 299) {
+      const error = await response.json();
+      setStatusMessage(error.error);
+    } else {
+      const message = await response.json();
+      setStatusMessage("Surway saved to database");
+      localStorage.setItem("name", message.name);
+      window.location.href = "/login/user";
+    }
+  };
+
   const handleSliderChange = (value: number, sliderName: string) => {
     setSurway((prevSurway) => ({
       ...prevSurway,
@@ -38,9 +62,23 @@ export function UserBoard(props: Props) {
   console.log(sliderValue);
   console.log(surway);
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e: MouseEvent) => {
+    e.preventDefault();
+
+    await sendData({
+      general_mood: surway.general_mood,
+      appetite: surway.appetite,
+      sleep: surway.sleep,
+      anxiety: surway.anxiety,
+      yourself_time: surway.yourself_time,
+      screen_time: surway.screen_time,
+    });
+
+    setSubmitted(true);
+    setError(false);
     console.log("SUBMITTED!!!!", surway);
   };
+
   return (
     <div>
       <p>Hello, {name}</p>
