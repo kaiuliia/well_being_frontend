2dd0c070f625547c11f3619fc728d2e4958b626e
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-20 10:45:51 +0100

    upd my register

--- src/components/registerform.tsx
+++ src/components/registerform.tsx
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 interface Props {
   title: string;
   // visible: boolean;
@@ -39,14 +53,13 @@ export function Register(props: Props) {
       localStorage.setItem("name", message.name);
     }
   };
-
+  const defaultTheme = createTheme();
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
-
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
@@ -71,27 +84,90 @@ export function Register(props: Props) {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit}
-        // style={{ display: props.visible ? "block" : "none" }}
-      >
-        <h3>Name:</h3>
-        <input name={"name"} onChange={handleNameChange} value={name} />
-        <br></br>
-        <h3>E-mail:</h3>
-        <input onChange={handleEmailChange} value={email} />
-        <br></br>
-        <h3>Create a password:</h3>
-        <input onChange={handlePasswordChange} value={password} />
-        <br></br>
-        <button className="btn" type="submit">
-          Submit
-        </button>
-        <p>{statusMessage}</p>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <p>{statusMessage}</p>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  name={"name"}
+                  onChange={handleNameChange}
+                  value={name}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={handleEmailChange}
+                  value={email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={handlePasswordChange}
+                  value={password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <p>{statusMessage}</p>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-// export default Register;
