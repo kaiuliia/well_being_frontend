798369e4676b511157f74572e286649db7a1bf50
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2024-02-17 20:49:42 +0100

    Upd header

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 export function HeaderDashboard() {
-  const element = <FontAwesomeIcon icon={faEnvelope} />;
   return (
     <div className="flex justify-between">
-      {element}
-      ICON RANGE SETTINGS
+      <FontAwesomeIcon icon={faCalendarDays} color="#BBC1CE" />
+      <FontAwesomeIcon icon={faChevronLeft} color="#A5BB5A" />
+      RANGE SETTINGS
+      <FontAwesomeIcon icon={faChevronRight} color="#A5BB5A" />
+      <FontAwesomeIcon icon={faSliders} color="#BBC1CE" />
     </div>
   );
 }
