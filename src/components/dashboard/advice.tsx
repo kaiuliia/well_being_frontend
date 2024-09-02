8c6951973ab8ad94989880f6213a8786a80786c1
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2024-09-02 20:26:57 +0200

    Add images.

--- src/components/dashboard/advice.tsx
+++ src/components/dashboard/advice.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
-  Carousel,
   Card,
   CardBody,
   CardFooter,
@@ -14,27 +13,41 @@ import {
   IonCardSubtitle,
   IonCardTitle,
 } from "@ionic/react";
+import { IonButton, IonContent, IonPopover } from "@ionic/react";
 import { useLocalStore } from "../../store/useStore";
 
 export function Advice() {
   const { dashboard } = useLocalStore();
   console.log("dash", dashboard);
 
+  const advice = {
+    sleep:
+      "Go to bed and wake up at the same time every day (even on weekends). This helps regulate your body's internal clock and improves the quality of your sleep." +
+      "Dark, Cool, and Quiet: Ensure your bedroom is dark (use blackout curtains if necessary), cool (ideally between 60-67°F or 15-19°C), and quiet (use earplugs or white noise machines if needed)." +
+      "Comfortable Bedding: Invest in a good mattress and pillows. Comfort is key to a restful night.",
+  };
+  // const popover = useRef<HTMLIonPopoverElement>(null);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const openPopover = (e: any) => {
+    // popover.current!.event = e;
+    setPopoverOpen(true);
+  };
   return (
     <>
-      <Carousel className="rounded-xl">
-        <p className={"text-white"}>hello its me</p>
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-      </Carousel>
+      <Button onClick={openPopover}>Click Me</Button>
+      {/*<IonPopover*/}
+      {/*  // ref={popover}*/}
+      {/*  isOpen={popoverOpen}*/}
+      {/*  onDidDismiss={() => setPopoverOpen(false)}*/}
+      {/*>*/}
+      {/*  <IonContent class="ion-padding absolute top-0">Hello World!</IonContent>*/}
+      {/*</IonPopover>*/}
+      <div className={"w-[100%] h-[100%] bg-black"}>
+        <div className=" w-full text-sm h-fit p-2 text-white absolute top-0 border-[1px] border-white-900">
+          {advice.sleep}
+        </div>
+      </div>
     </>
   );
 }
