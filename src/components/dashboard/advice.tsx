8360da6aa8b6c445d5ae6738a07d35a5af2120fd
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2025-01-12 20:13:57 +0100

    Fix advice types.

--- src/components/dashboard/advice.tsx
+++ src/components/dashboard/advice.tsx
@@ -5,12 +5,16 @@ import { useLocalStore } from "../../store/useStore";
 
 export function Advice() {
   const { advicesArray } = useLocalStore();
-  console.log("advicesArray", advicesArray);
+  console.log(advicesArray[0].title);
   return (
     <>
       <div className={"flex flex-row py-7 gap-5"}>
         {advicesArray.map((element, index) => (
-          <AdviceButton key={index} moodType={element} />
+          <AdviceButton
+            key={index}
+            title={element.title}
+            description={element.advices}
+          />
         ))}
       </div>
     </>
